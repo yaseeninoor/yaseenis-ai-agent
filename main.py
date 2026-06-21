f61bcf2367cfdc87d71f07822ea20511cb5968ad
yaseeninoor <55235825+yaseeninoor@users.noreply.github.com>
2026-06-21 04:03:31 +0400

    Update PDF loader to include more file patterns

--- main.py
+++ main.py
@@ -1,4 +1,2 @@
-# This tells the AI to look inside the 'pdfs' folder for all .pdf files
-loader = DirectoryLoader('./pdfs', glob="./*.pdf", loader_cls=PyPDFLoader)
-documents = loader.load()
-
+# Update this line in main.py
+loader = DirectoryLoader('./pdfs', glob="**/*.[pd][dfx]*", loader_cls=PyPDFLoader)
